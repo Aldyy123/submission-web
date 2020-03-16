f40c9321a80443cd54dd9395634fabb3cdedcc9a
Aldyy <bocah.ngik@gmail.com>
2020-03-16 07:05:38 +0700

    INi Submission Dicoding

---
+++ script.js
@@ -0,0 +1,9 @@
+function kirim() {
+    var nama = document.getElementById("nama").value;
+    var pesan = document.getElementById("pesan").value;
+    alert("Komentar ada di console log");
+    console.log("Namanya : " + nama);
+    console.log("Pesannya : " + pesan);
+
+    console.log("Terimakasih");
+}
